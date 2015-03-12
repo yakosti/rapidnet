84b48561b28d444f026e1aec78401c2494eac486
Lay Kuan Loh <lkloh@Lays-MBP.wv.cc.cmu.edu>
2015-03-12 13:27:54 -0400

    bugs

---
+++ build/debug/ns3/pktfwd-prov-module.h
@@ -0,0 +1,12 @@
+
+#ifdef NS3_MODULE_COMPILATION
+# error "Do not include ns3 module aggregator headers from other modules; these are meant only for end user scripts."
+#endif
+
+#ifndef NS3_MODULE_PKTFWD_PROV
+    
+
+// Module headers:
+#include "pktfwd-prov-helper.h"
+#include "pktfwd-prov.h"
+#endif
