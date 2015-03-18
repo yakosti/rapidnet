651b3c8941b1a5b5d435266091664f3ef784d2af
Lay Kuan Loh <lkloh@Lays-MBP.wv.cc.cmu.edu>
2015-03-18 14:25:42 -0400

    added generalized check sat and test code

--- src/rapidnet-compiler/sdn-test.h
+++ src/rapidnet-compiler/sdn-test.h
@@ -241,6 +241,27 @@ ConsList create_constraint_list_five() {
   return clist;
 }
 
+FormList create_formula_list_five() {
+  vector<Variable::TypeCode> domain_types;
+  domain_types.push_back(Variable::STRING);
+  FunctionSchema* mother_schema = new FunctionSchema("mother", domain_types, Variable::STRING);
+
+  //assert that MichelleObama is the mother of MaliaObama
+  vector<Term*> args;
+  StringVal* MaliaObama = new StringVal("MaliaObama");
+  args.push_back(MaliaObama);
+  UserFunction* mother_malia = new UserFunction(mother_schema, args);
+
+  StringVal* MichelleObama = new StringVal("MichelleObama");
+  StringVal* BarbaraBush = new StringVal("BarbaraBush");
+
+  Constraint* michelle_is_mother_of_malia = new Constraint(Constraint::EQ, mother_malia, MichelleObama);
+
+  FormList flist;
+  flist.push_back(michelle_is_mother_of_malia);
+  return flist;
+}
+
 
 /*
  * CVC4
@@ -287,6 +308,24 @@ void test_check_sat() {
   ConsList clist6;
   FormList flist6 = create_formula_list_six();
   map<Variable*, int> mymap6 = check_sat(clist6, flist6);
+}
+
+void test_check_sat_generalized() {
+  cout << "\n==== Testing Check Sat Generalized - exists y,forall x,((0<x AND x<5)=>(y>x)) ====\n";
+  FormList flist2 = create_formula_list_one();
+  map<Variable*, int> mymap2 = check_sat_generalized(flist2);
+
+  cout << "\n========= Testing Check Sat Generalized - (x>y)and(y>z)=>(x>z) ==========\n";
+  FormList flist4 = create_formula_list_four();
+  map<Variable*, int> mymap4 = check_sat_generalized(flist4);
+
+  cout << "\n============ Testing Check Sat Generalized - UserFunction ==========\n";
+  FormList flist5 = create_formula_list_five();
+  map<Variable*, int> mymap5 = check_sat_generalized(flist5);
+
+  cout << "\n============ Testing Check Sat Generalized - QuantifyPredicate ==========\n";
+  FormList flist6 = create_formula_list_six();
+  map<Variable*, int> mymap6 = check_sat_generalized(flist6);
 }
 
 /*
@@ -693,6 +732,7 @@ void test_parsing() {
   nested_function_check();
   
   test_check_sat();
+  test_check_sat_generalized();
 }
 
 /*
