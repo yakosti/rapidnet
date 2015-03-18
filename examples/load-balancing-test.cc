db1375e562b9a30de6ab47751e480376d7eed838
Lay Kuan Loh <lkloh@Lays-MBP.wv.cc.cmu.edu>
2015-03-18 12:35:13 -0400

    client packet

--- examples/load-balancing-test.cc
+++ examples/load-balancing-test.cc
@@ -14,17 +14,56 @@
 #include <sstream>
 #include <stdio.h>
 
-//Openflow connections
-// #define ofconn(host1, host2)\
-//   tuple(SdnMacLearning::OFCONN,			\
-// 	attr("ofconn_attr1", Ipv4Value, host1),\
-// 	attr("ofconn_attr2", Ipv4Value, host2))
-
-// #define insertofconn(host1, host2)			\
-//   app(host1) -> Insert (ofconn(addr(host1), addr(host2)));
-
-/* **************************************************************** *
+/* 
+ * **************************************************************** *
  *                                                                  *
+ *                            DEFINE TUPLES                         *
+ *                                                                  *
+ * **************************************************************** *
+ */
+
+
+/* switch connection */
+
+#define switchConnection(Switch1, Switch2) \
+  tuple(LoadBalancing::SWITCHCONNECTION, \
+    attr("switchConnection_attr1", Ipv4Value, Switch1),\
+    attr("switchConnection_attr2", Ipv4Value, Switch2))
+
+#define insert_switchConnection(Switch1, Switch2) \
+  app(Switch1) -> Insert(switchConnection(addr(Switch1),addr(Switch2)));
+
+/* pktClient */
+
+#define pktClient(Switch, Client) \
+  tuple(LoadBalancing::PKTCLIENT, \
+    attr("pktClient_attr1", Ipv4Value, Switch),\
+    attr("pktClient_attr2", Ipv4Value, Client))
+
+#define insert_pktClient(Switch, Client) \
+  app(Switch) -> Insert(pktClient(addr(Switch),add(Client)));
+
+/* 
+ * **************************************************************** *
+ *                                                                  *
+ *                            DEFINE TUPLES                         *
+ *                                                                  *
+ * **************************************************************** *
+ */
+
+
+
+
+
+
+
+
+/* 
+ * **************************************************************** *
+ *                                                                  *
+ *                            SIMULATION                            *
+ *                                                                  *
+ * **************************************************************** *
  */
 
 #define nodeNum 4
@@ -58,6 +97,17 @@ int main(int argc, char *argv[])
   Simulator::Destroy();
   return 0;
 }
+
+/* 
+ * **************************************************************** *
+ *                                                                  *
+ *                            SIMULATION                            *
+ *                                                                  *
+ * **************************************************************** *
+ */
+
+
+
 
 
 
