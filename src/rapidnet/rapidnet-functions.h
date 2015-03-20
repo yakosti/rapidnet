9998d999c8086575cf33bd85f59451e178834f74
Lay Kuan Loh <lkloh@Lays-MBP.wv.cc.cmu.edu>
2015-03-20 13:57:37 -0400

    added back functions

--- src/rapidnet/rapidnet-functions.h
+++ src/rapidnet/rapidnet-functions.h
@@ -23,6 +23,22 @@
 #include <list>
 #include "expression.h"
 #include "tuple.h"
+#include "rapidnet-application-base.h"
+#include "evp-key.h"
+#include "ns3/ptr.h"
+#include "sendlog-authentication-manager.h"
+#include "ns3/log.h"
+#include "openssl/pem.h"
+#include "ns3/byte-array-value.h"
+#include "openssl/evp.h"
+#include "openssl/hmac.h"
+
+#define CLOG "clog"
+#define COUT "cout"
+#define __GetStream(stream_name) \
+    (stream_name==COUT? cout: \
+    (stream_name==CLOG? clog: \
+     clog))
 
 using namespace std;
 
@@ -48,11 +64,26 @@ public:
 
   virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
 
-  static Ptr<FunctionExpr> New (Ptr<Expression> source);
+  static Ptr<FunctionExpr> New (Ptr<Expression> source, Ptr<RapidNetApplicationBase> app = NULL);
 
 protected:
 
   Ptr<Expression> m_source;
+};
+
+class FPrepend : public FunctionExpr
+{
+public:
+
+  virtual ~FPrepend () {}
+
+  virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
+
+  static Ptr<FunctionExpr> New (Ptr<Expression> value1, Ptr<Expression> listvalue1, Ptr<RapidNetApplicationBase> app = NULL);
+
+protected:
+
+  Ptr<Expression> m_value, m_listvalue;
 };
 
 class FEmpty : public FunctionExpr
@@ -63,7 +94,7 @@ public:
 
   virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
 
-  static Ptr<FunctionExpr> New ();
+  static Ptr<FunctionExpr> New (Ptr<RapidNetApplicationBase> app = NULL);
 
 };
 
@@ -80,7 +111,7 @@ public:
 
   virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
 
-  static Ptr<FunctionExpr> New (Ptr<Expression> head, Ptr<Expression> tail);
+  static Ptr<FunctionExpr> New (Ptr<Expression> head, Ptr<Expression> tail, Ptr<RapidNetApplicationBase> app = NULL);
 
 protected:
 
@@ -95,7 +126,7 @@ public:
 
   virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
 
-  static Ptr<FunctionExpr> New (Ptr<Expression> lst, Ptr<Expression> index);
+  static Ptr<FunctionExpr> New (Ptr<Expression> lst, Ptr<Expression> index, Ptr<RapidNetApplicationBase> app = NULL);
 
 protected:
 
@@ -116,7 +147,7 @@ public:
 
   virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
 
-  static Ptr<FunctionExpr> New (Ptr<Expression> lst, Ptr<Expression> item);
+  static Ptr<FunctionExpr> New (Ptr<Expression> lst, Ptr<Expression> item, Ptr<RapidNetApplicationBase> app = NULL);
 
 protected:
 
@@ -136,7 +167,7 @@ public:
 
   virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
 
-  static Ptr<FunctionExpr> New ();
+  static Ptr<FunctionExpr> New (Ptr<RapidNetApplicationBase> app = NULL);
 };
 
 /**
@@ -153,7 +184,7 @@ public:
 
   virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
 
-  static Ptr<FunctionExpr> New (Ptr<Expression> time2, Ptr<Expression> time1);
+  static Ptr<FunctionExpr> New (Ptr<Expression> time2, Ptr<Expression> time1, Ptr<RapidNetApplicationBase> app = NULL);
 
 protected:
 
@@ -175,7 +206,7 @@ public:
   virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
 
   static Ptr<FunctionExpr> New (Ptr<Expression> time,
-    Ptr<Expression> hslsPeriod);
+    Ptr<Expression> hslsPeriod, Ptr<RapidNetApplicationBase> app = NULL);
 
 protected:
 
@@ -195,7 +226,7 @@ public:
 
   virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
 
-  static Ptr<FunctionExpr> New (Ptr<Expression> listAttrName);
+  static Ptr<FunctionExpr> New (Ptr<Expression> listAttrName, Ptr<RapidNetApplicationBase> app = NULL);
 
 protected:
 
@@ -215,7 +246,7 @@ public:
 
   virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
 
-  static Ptr<FunctionExpr> New (Ptr<Expression> listAttrName);
+  static Ptr<FunctionExpr> New (Ptr<Expression> listAttrName, Ptr<RapidNetApplicationBase> app = NULL);
 
 protected:
 
@@ -236,12 +267,63 @@ public:
 
   virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
 
-  static Ptr<FunctionExpr> New (Ptr<Expression> listAttrName);
+  static Ptr<FunctionExpr> New (Ptr<Expression> listAttrName, Ptr<RapidNetApplicationBase> app = NULL);
 
 protected:
 
   Ptr<Expression> m_listAttrName;
 };
+
+
+
+
+
+
+/**
+ * \ingroup rapidnet_functions
+ *
+ * \brief A RapidNet function that returns the first element of a list.
+ */
+
+class FFirst : public FunctionExpr
+{
+public:
+
+  virtual ~FFirst () {}
+
+  virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
+
+  static Ptr<FunctionExpr> New (Ptr<Expression> listAttrName, Ptr<RapidNetApplicationBase> app = NULL);
+
+protected:
+
+  Ptr<Expression> m_listAttrName;
+};
+
+
+/**
+ * \ingroup rapidnet_functions
+ *
+ * \brief A RapidNet function that removes and returns first element of a list.
+ */
+class FRemoveFirst : public FunctionExpr
+{
+public:
+
+  virtual ~FRemoveFirst () {}
+
+  virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
+
+  static Ptr<FunctionExpr> New (Ptr<Expression> listAttrName, Ptr<RapidNetApplicationBase> app = NULL);
+
+protected:
+
+  Ptr<Expression> m_listAttrName;
+};
+
+
+
+
 
 
 /**
@@ -258,7 +340,7 @@ public:
 
   virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
 
-  static Ptr<FunctionExpr> New (Ptr<Expression> arg);
+  static Ptr<FunctionExpr> New (Ptr<Expression> arg, Ptr<RapidNetApplicationBase> app = NULL);
 
 protected:
 
@@ -278,7 +360,7 @@ public:
 
   virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
 
-  static Ptr<FunctionExpr> New ();
+  static Ptr<FunctionExpr> New (Ptr<RapidNetApplicationBase> app = NULL);
 };
 
 
@@ -296,7 +378,7 @@ public:
 
   virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
 
-  static Ptr<FunctionExpr> New (Ptr<Expression> arg);
+  static Ptr<FunctionExpr> New (Ptr<Expression> arg, Ptr<RapidNetApplicationBase> app = NULL);
 
 protected:
 
@@ -318,7 +400,7 @@ public:
 
   virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
 
-  static Ptr<FunctionExpr> New ();
+  static Ptr<FunctionExpr> New (Ptr<RapidNetApplicationBase> app = NULL);
 };
 
 /**
@@ -340,7 +422,8 @@ public:
   virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
 
   static Ptr<FunctionExpr> New (Ptr<Expression> svExpr,
-                                Ptr<Expression> strExpr);
+                                Ptr<Expression> strExpr,
+                                Ptr<RapidNetApplicationBase> app = NULL);
 protected:
 
   Ptr<Expression> m_svExpr, m_strExpr;
@@ -365,7 +448,8 @@ public:
   virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
 
   static Ptr<FunctionExpr> New (Ptr<Expression> svExpr_1,
-                                Ptr<Expression> svExpr_2);
+                                Ptr<Expression> svExpr_2,
+                                Ptr<RapidNetApplicationBase> app = NULL);
 protected:
 
   Ptr<Expression> m_svExpr_1, m_svExpr_2;
@@ -387,7 +471,8 @@ public:
   virtual ~FSvAppend () {}
   virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
   static Ptr<FunctionExpr> New (Ptr<Expression> svExpr,
-                                Ptr<Expression> strExpr);
+                                Ptr<Expression> strExpr,
+                                Ptr<RapidNetApplicationBase> app = NULL);
 private:
   Ptr<Expression> m_svExpr, m_strExpr;
 };
@@ -408,7 +493,8 @@ public:
   virtual ~FSvRemove () {}
   virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
   static Ptr<FunctionExpr> New (Ptr<Expression> svExpr,
-                                Ptr<Expression> strExpr);
+                                Ptr<Expression> strExpr,
+                                Ptr<RapidNetApplicationBase> app = NULL);
 private:
   Ptr<Expression> m_svExpr, m_strExpr;
 };
@@ -418,7 +504,7 @@ class FPEdb : public FunctionExpr
 public:
   virtual ~FPEdb () {}
   virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
-  static Ptr<FunctionExpr> New (Ptr<Expression> prov, Ptr<Expression> id);
+  static Ptr<FunctionExpr> New (Ptr<Expression> prov, Ptr<Expression> id, Ptr<RapidNetApplicationBase> app = NULL);
 
 private:
   Ptr<Expression> m_prov, m_id;
@@ -429,7 +515,7 @@ class FPIdb : public FunctionExpr
 public:
   virtual ~FPIdb () {}
   virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
-  static Ptr<FunctionExpr> New (Ptr<Expression> provList, Ptr<Expression> loc);
+  static Ptr<FunctionExpr> New (Ptr<Expression> provList, Ptr<Expression> loc, Ptr<RapidNetApplicationBase> app = NULL);
 
 private:
   Ptr<Expression> m_provList, m_loc;
@@ -440,13 +526,124 @@ class FPRule : public FunctionExpr
 public:
   virtual ~FPRule () {}
   virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
-  static Ptr<FunctionExpr> New (Ptr<Expression> provList, Ptr<Expression> rloc,Ptr<Expression> rule);
+  static Ptr<FunctionExpr> New (Ptr<Expression> provList, Ptr<Expression> rloc,Ptr<Expression> rule, Ptr<RapidNetApplicationBase> app = NULL);
 
 private:
   Ptr<Expression> m_provList, m_rloc, m_rule;
 };
 
+
+class FAppend2 : public FunctionExpr
+{
+public:
+
+  virtual ~FAppend2 () {}
+
+  virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
+
+  static Ptr<FunctionExpr> New (Ptr<Expression> source, Ptr<Expression> list, Ptr<RapidNetApplicationBase> app = NULL);
+
+protected:
+
+  Ptr<Expression> m_source,  m_listvalue;
+};
+
+
+/* 
+ * Jul 11, 2012
+ * Cheng Luo 
+*/
+class FSign: public FunctionExpr
+{
+public:
+
+  virtual ~FSign () {}
+
+  virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
+
+  static Ptr<FunctionExpr> New(Ptr<Expression> list1, Ptr<Expression> key1, Ptr<RapidNetApplicationBase> app= NULL);
+
+protected:
+
+  Ptr<Expression> m_list;
+  Ptr<Expression> m_privateKey;
+};
+
+class FVerify: public FunctionExpr
+{
+public:
+
+  virtual ~FVerify () {}
+
+  virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
+
+  static Ptr<FunctionExpr> New(Ptr<Expression> list1, Ptr<Expression> signature, Ptr<Expression> publicKey, Ptr<RapidNetApplicationBase> app= NULL);
+
+protected:
+
+  Ptr<Expression> m_signature, m_list;
+  Ptr<Expression> m_publicKey;
+  //Ptr<Expression> m_node, m_node1;
+};
+
+class FMAC : public FunctionExpr
+{
+public:
+  virtual ~FMAC () {}
+
+  virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
+
+  static Ptr<FunctionExpr> New (Ptr<Expression> message, Ptr<Expression> secretKey, Ptr<RapidNetApplicationBase> app = NULL);
+
+protected: 
+  Ptr<Expression> m_message, m_secretKey;
+};
+
+
+class FReverse : public FunctionExpr
+{
+public:
+  virtual ~FReverse () {}
+
+  Ptr<Value> Eval (Ptr<Tuple> tuple);
+
+  static Ptr<FunctionExpr> New (Ptr<Expression> list, Ptr<RapidNetApplicationBase> app = NULL);
+
+protected: 
+  Ptr<Expression> m_list;
+};
+
+class FGet : public FunctionExpr
+{
+public:
+  virtual ~FGet () {}
+
+  Ptr<Value> Eval (Ptr<Tuple> tuple);
+
+  static Ptr<FunctionExpr> New (Ptr<Expression> list, Ptr<Expression> i, Ptr<RapidNetApplicationBase> app = NULL);
+
+protected: 
+  Ptr<Expression> m_list;
+  Ptr<Expression> m_i;
+};
+
+
+class FVerifyMac : public FunctionExpr
+{
+public:
+  virtual ~FVerifyMac () {}
+
+  virtual Ptr<Value> Eval (Ptr<Tuple> tuple);
+
+  static Ptr<FunctionExpr> New(Ptr<Expression> msg, Ptr<Expression> mac, Ptr<Expression> skey, Ptr<RapidNetApplicationBase> app= NULL);
+
+protected:
+  Ptr<Expression> m_message, m_mac, m_secretKey;
+};
+
+
 } // namespace rapidnet
 } // namespace ns3
 
 #endif // FUNCTIONS_H
+
